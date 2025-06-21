f6f93fce9137905a1a33ced5ccac04733a922286
Marcel Wagner <marcel.alex.wagner@outlook.com>
2025-06-21 18:20:36 +0200

    Fix test build errors

--- tests/IndentRainbow.Logic.Tests/Classification/AlternatingLineDecoratorTests.cs
+++ tests/IndentRainbow.Logic.Tests/Classification/AlternatingLineDecoratorTests.cs
@@ -9,153 +9,160 @@ using static IndentRainbow.Logic.Parser.ColorParser;
 
 namespace IndentRainbow.Logic.Tests.Classification
 {
-	[TestClass]
-	public class AlternatingLineDecoratorTests
-	{
-		/// <summary>
-		/// Four Space Indent constant;
-		/// This name was chosen for easy creation of tests
-		/// </summary>
-		private const string FSI = "    ";
-		private const string TABI = "\t";
+    [TestClass]
+    public class AlternatingLineDecoratorTests
+    {
+        /// <summary>
+        /// Four Space Indent constant;
+        /// This name was chosen for easy creation of tests
+        /// </summary>
+        private const string FSI = "    ";
+        private const string TABI = "\t";
 
-		private AlternatingLineDecorator decorator;
-		private readonly Mock<IBackgroundTextIndexDrawer> backgroundTextIndexDrawerMock = new Mock<IBackgroundTextIndexDrawer>();
-		private IBackgroundTextIndexDrawer backgroundTextIndexDrawer => backgroundTextIndexDrawerMock.Object;
-		private readonly IndentValidator validator = new IndentValidator(4);
-		private readonly RainbowBrushGetter rainbowgetter = new RainbowBrushGetter(new Color[] {
-				Color.FromRgb(0, 0, 0)
-			}, new SolidColorBrush(), ColorMode.Solid, false);
-
-
-		[TestInitialize]
-		public void Setup()
-		{
-			decorator = new AlternatingLineDecorator(backgroundTextIndexDrawer, rainbowgetter, validator);
-		}
-
-		[DataTestMethod]
-		[DataRow(FSI + FSI + TABI + FSI + "t", new int[] { 0, 4, 8, 9, 13 })]
-		[DataRow(TABI + FSI + "123456789", new int[] { 0, 1, 5 })]
-		[DataRow(TABI + "123456789", new int[] { 0, 1 })]
-		[DataRow(TABI + TABI + TABI + "123456789", new int[] { 0, 1, 2, 3 })]
-		[DataRow(TABI + "1", new int[] { 0, 1 })]
-		[DataRow(TABI + TABI + TABI + "1", new int[] { 0, 1, 2, 3 })]
-		[DataRow(TABI + TABI + TABI + TABI + TABI + TABI + TABI + "1", new int[] { 0, 1, 2, 3, 4, 5, 6, 7 })]
-		[DataRow(FSI + "text" + FSI, new int[] { 0 })]
-		[DataRow("", new int[] { })]
-		[DataRow(FSI + FSI + "12345", new int[] { 0, 4 })]
-		public void DecorateLineTests_IndexTesting_ExpectedBehavior(string text, int[] spans)
-		{
-			var randomDrawIndexCutoff = new Random().Next(2 << 16);
-			decorator.DecorateLine(text, randomDrawIndexCutoff);
-
-			if(spans.Length > 0)
-			{
-				for (var i = 0; i < spans.Length - 1; i++)
-				{
-					backgroundTextIndexDrawerMock.Verify(
-						p => p.DrawBackground(
-							spans[i] + randomDrawIndexCutoff, 
-							It.IsIn(new int[] { FSI.Length, TABI.Length }),
-							It.IsAny<Brush>()),
-						Times.Once()
-					);
-				}
-			}
-			else
-			{
-				backgroundTextIndexDrawerMock.Verify(
-					p => p.DrawBackground(
-							It.IsAny<int>(),
-							It.IsAny<int>(),
-							It.IsAny<Brush>()
-						),
-					Times.Never()
-				);
-			}
-		}
-
-		[DataTestMethod]
-		[DataRow(FSI + FSI + FSI + FSI + FSI + FSI + FSI)]
-		[DataRow(FSI + "dsadsadsa")]
-		[DataRow(FSI + "  dsadsa")]
-		[DataRow(TABI + FSI + "  dsadsa")]
-		[DataRow(FSI + TABI + "  dsadsa")]
-		public void DecorateLineTests_ColorTesting_ExpectedBehavior(string text)
-		{
-			var sequence = new MockSequence();
-			for (var i = 0; i < text.Length / FSI.Length; i++)
-			{
-				backgroundTextIndexDrawerMock.InSequence(sequence).Setup(
-					p => p.DrawBackground(
-						It.IsAny<int>(),
-						It.IsAny<int>(),
-						It.IsAny<Brush>()
-					)
-				);
-			}
-
-			decorator.DecorateLine(text, 0);
-
-			for (var i = 0; i < text.Length / FSI.Length; i++)
-			{
-				backgroundTextIndexDrawerMock.InSequence(sequence).Setup(
-					p => p.DrawBackground(
-						It.IsAny<int>(),
-						It.IsAny<int>(),
-						rainbowgetter.GetColorByIndex(i, 0)
-					)
-				);
-			}
-		}
+        private AlternatingLineDecorator decorator;
+        private readonly Mock<IBackgroundTextIndexDrawer> backgroundTextIndexDrawerMock = new Mock<IBackgroundTextIndexDrawer>();
+        private IBackgroundTextIndexDrawer backgroundTextIndexDrawer => backgroundTextIndexDrawerMock.Object;
+        private readonly IndentValidator validator = new IndentValidator(4);
+        private readonly RainbowBrushGetter rainbowgetter = new RainbowBrushGetter(new Color[] {
+                Color.FromRgb(0, 0, 0)
+            }, new SolidColorBrush(), ColorMode.Solid, false);
 
 
-		[DataTestMethod]
-		[DataRow(FSI + FSI + TABI + FSI + " t", 14)]
-		[DataRow(TABI + FSI + " 123456789", 6)]
-		[DataRow(FSI + " text" + FSI, 5)]
-		[DataRow(FSI + FSI + " 12345", 9)]
-		public void DecorateLineTests_IndexTesting_ErrorBehaviors(string text, int endOffset)
-		{
-			var randomDrawIndexCutoff = new Random().Next(2 << 16);
-			decorator.DecorateLine(text, randomDrawIndexCutoff);
+        [TestInitialize]
+        public void Setup()
+        {
+            decorator = new AlternatingLineDecorator(backgroundTextIndexDrawer, rainbowgetter, validator);
+        }
 
-			backgroundTextIndexDrawerMock.Verify(
-				p => p.DrawBackground(
-					randomDrawIndexCutoff,
-					endOffset,
-					It.IsAny<Brush>()),
-				Times.Once()
-			);
-			backgroundTextIndexDrawerMock.Verify(
-				p => p.DrawBackground(
-						It.IsNotIn(new int[] { randomDrawIndexCutoff, endOffset }),
-						It.IsNotIn(4),
-						It.IsAny<Brush>()
-					),
-				Times.Never()
-			);
-		}
+        [DataTestMethod]
+        [DataRow(FSI + FSI + TABI + FSI + "t", new int[] { 0, 4, 8, 9, 13 })]
+        [DataRow(TABI + FSI + "123456789", new int[] { 0, 1, 5 })]
+        [DataRow(TABI + "123456789", new int[] { 0, 1 })]
+        [DataRow(TABI + TABI + TABI + "123456789", new int[] { 0, 1, 2, 3 })]
+        [DataRow(TABI + "1", new int[] { 0, 1 })]
+        [DataRow(TABI + TABI + TABI + "1", new int[] { 0, 1, 2, 3 })]
+        [DataRow(TABI + TABI + TABI + TABI + TABI + TABI + TABI + "1", new int[] { 0, 1, 2, 3, 4, 5, 6, 7 })]
+        [DataRow(FSI + "text" + FSI, new int[] { 0 })]
+        [DataRow("", new int[] { })]
+        [DataRow(FSI + FSI + "12345", new int[] { 0, 4 })]
+        public void DecorateLineTests_IndexTesting_ExpectedBehavior(string text, int[] spans)
+        {
+            var randomDrawIndexCutoff = new Random().Next(2 << 16);
+            decorator.DecorateLine(text, randomDrawIndexCutoff, 0);
 
-		[DataTestMethod]
-		[DataRow(FSI + FSI + TABI + FSI + " t", 4)]
-		[DataRow(TABI + FSI + " 123456789", 1)]
-		[DataRow(FSI + " text" + FSI, 4)]
-		[DataRow(FSI + FSI + " 12345", 4)]
-		public void DecorateLineTests_NoErrorDetection_ErrorBehaviors(string text, int endOffset)
-		{
-			decorator.detectErrors = false;
-			var randomDrawIndexCutoff = new Random().Next(2 << 16);
-			decorator.DecorateLine(text, randomDrawIndexCutoff);
+            if (spans.Length > 0)
+            {
+                for (var i = 0; i < spans.Length - 1; i++)
+                {
+                    backgroundTextIndexDrawerMock.Verify(
+                        p => p.DrawBackground(
+                            spans[i] + randomDrawIndexCutoff,
+                            It.IsIn(new int[] { FSI.Length, TABI.Length }),
+                            It.IsAny<Brush>(),
+                        It.IsAny<int>()),
+                        Times.Once()
+                    );
+                }
+            }
+            else
+            {
+                backgroundTextIndexDrawerMock.Verify(
+                    p => p.DrawBackground(
+                            It.IsAny<int>(),
+                            It.IsAny<int>(),
+                            It.IsAny<Brush>(),
+                        It.IsAny<int>()
+                        ),
+                    Times.Never()
+                );
+            }
+        }
 
-			backgroundTextIndexDrawerMock.Verify(
-				p => p.DrawBackground(
-					randomDrawIndexCutoff,
-					endOffset,
-					It.IsAny<Brush>()),
-				Times.Once()
-			);
-		}
-	}
+        [DataTestMethod]
+        [DataRow(FSI + FSI + FSI + FSI + FSI + FSI + FSI)]
+        [DataRow(FSI + "dsadsadsa")]
+        [DataRow(FSI + "  dsadsa")]
+        [DataRow(TABI + FSI + "  dsadsa")]
+        [DataRow(FSI + TABI + "  dsadsa")]
+        public void DecorateLineTests_ColorTesting_ExpectedBehavior(string text)
+        {
+            var sequence = new MockSequence();
+            for (var i = 0; i < text.Length / FSI.Length; i++)
+            {
+                backgroundTextIndexDrawerMock.InSequence(sequence).Setup(
+                    p => p.DrawBackground(
+                        It.IsAny<int>(),
+                        It.IsAny<int>(),
+                        It.IsAny<Brush>(),
+                        It.IsAny<int>()
+                    )
+                );
+            }
+
+            decorator.DecorateLine(text, 0, 0);
+
+            for (var i = 0; i < text.Length / FSI.Length; i++)
+            {
+                backgroundTextIndexDrawerMock.InSequence(sequence).Setup(
+                    p => p.DrawBackground(
+                        It.IsAny<int>(),
+                        It.IsAny<int>(),
+                        rainbowgetter.GetColorByIndex(i, 0),
+                        It.IsAny<int>()
+                    )
+                );
+            }
+        }
+
+
+        [DataTestMethod]
+        [DataRow(FSI + FSI + TABI + FSI + " t", 14)]
+        [DataRow(TABI + FSI + " 123456789", 6)]
+        [DataRow(FSI + " text" + FSI, 5)]
+        [DataRow(FSI + FSI + " 12345", 9)]
+        public void DecorateLineTests_IndexTesting_ErrorBehaviors(string text, int endOffset)
+        {
+            var randomDrawIndexCutoff = new Random().Next(2 << 16);
+            decorator.DecorateLine(text, randomDrawIndexCutoff, 0);
+
+            backgroundTextIndexDrawerMock.Verify(
+                p => p.DrawBackground(
+                    randomDrawIndexCutoff,
+                    endOffset,
+                    It.IsAny<Brush>(),
+                        It.IsAny<int>()),
+                Times.Once()
+            );
+            backgroundTextIndexDrawerMock.Verify(
+                p => p.DrawBackground(
+                        It.IsNotIn(new int[] { randomDrawIndexCutoff, endOffset }),
+                        It.IsNotIn(4),
+                        It.IsAny<Brush>(),
+                        It.IsAny<int>()
+                    ),
+                Times.Never()
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow(FSI + FSI + TABI + FSI + " t", 4)]
+        [DataRow(TABI + FSI + " 123456789", 1)]
+        [DataRow(FSI + " text" + FSI, 4)]
+        [DataRow(FSI + FSI + " 12345", 4)]
+        public void DecorateLineTests_NoErrorDetection_ErrorBehaviors(string text, int endOffset)
+        {
+            decorator.detectErrors = false;
+            var randomDrawIndexCutoff = new Random().Next(2 << 16);
+            decorator.DecorateLine(text, randomDrawIndexCutoff, 0);
+
+            backgroundTextIndexDrawerMock.Verify(
+                p => p.DrawBackground(
+                    randomDrawIndexCutoff,
+                    endOffset,
+                    It.IsAny<Brush>(),
+                        It.IsAny<int>()),
+                Times.Once()
+            );
+        }
+    }
 }
--- tests/IndentRainbow.Logic.Tests/Classification/MonocolorLineDecoratorTests.cs
+++ tests/IndentRainbow.Logic.Tests/Classification/MonocolorLineDecoratorTests.cs
@@ -9,154 +9,161 @@ using static IndentRainbow.Logic.Parser.ColorParser;
 
 namespace IndentRainbow.Logic.Tests.Classification
 {
-	[TestClass]
-	public class MonoColorLineDecoratorTests
-	{
-		/// <summary>
-		/// Four Space Indent constant;
-		/// This name was chosen for easy creation of tests
-		/// </summary>
-		private const string FSI = "    ";
-		private const string TABI = "\t";
+    [TestClass]
+    public class MonoColorLineDecoratorTests
+    {
+        /// <summary>
+        /// Four Space Indent constant;
+        /// This name was chosen for easy creation of tests
+        /// </summary>
+        private const string FSI = "    ";
+        private const string TABI = "\t";
 
-		private MonocolorLineDecorator decorator;
-		private readonly Mock<IBackgroundTextIndexDrawer> backgroundTextIndexDrawerMock = new Mock<IBackgroundTextIndexDrawer>();
-		private IBackgroundTextIndexDrawer BackgroundTextIndexDrawer => backgroundTextIndexDrawerMock.Object;
-		private readonly IndentValidator validator = new IndentValidator(4);
-		private readonly RainbowBrushGetter rainbowgetter = new RainbowBrushGetter(new Color[] {
-				Color.FromRgb(0,0,0)
-			}, new SolidColorBrush(), ColorMode.Solid, false);
+        private MonocolorLineDecorator decorator;
+        private readonly Mock<IBackgroundTextIndexDrawer> backgroundTextIndexDrawerMock = new Mock<IBackgroundTextIndexDrawer>();
+        private IBackgroundTextIndexDrawer BackgroundTextIndexDrawer => backgroundTextIndexDrawerMock.Object;
+        private readonly IndentValidator validator = new IndentValidator(4);
+        private readonly RainbowBrushGetter rainbowgetter = new RainbowBrushGetter(new Color[] {
+                Color.FromRgb(0,0,0)
+            }, new SolidColorBrush(), ColorMode.Solid, false);
 
-		[TestInitialize]
-		public void Setup()
-		{
-			decorator = new MonocolorLineDecorator(BackgroundTextIndexDrawer, rainbowgetter, validator);
-		}
+        [TestInitialize]
+        public void Setup()
+        {
+            decorator = new MonocolorLineDecorator(BackgroundTextIndexDrawer, rainbowgetter, validator);
+        }
 
-		[DataTestMethod]
-		[DataRow(FSI + FSI + TABI + FSI + "t", 13, 3)]
-		[DataRow(TABI + FSI + "123456789", 5, 1)]
-		[DataRow(TABI + "123456789", 1, 0)]
-		[DataRow(TABI + TABI + TABI + "123456789", 3, 2)]
-		[DataRow(TABI + "1", 1, 0)]
-		[DataRow(TABI + TABI + TABI + "1", 3, 2)]
-		[DataRow(TABI + TABI + TABI + TABI + TABI + TABI + TABI + "1", 7, 6)]
-		[DataRow(FSI + "text" + FSI, 4, 0)]
-		[DataRow("", null, -1)]
-		[DataRow(FSI + FSI + "12345", 8, 1)]
-		public void DecorateLineTests_IndexTesting_ExpectedBehavior(string text, int? possibleLength, int colorIndex)
-		{
-			var randomDrawIndexCutoff = new Random().Next(2 << 16);
-			decorator.DecorateLine(text, randomDrawIndexCutoff);
+        [DataTestMethod]
+        [DataRow(FSI + FSI + TABI + FSI + "t", 13, 3)]
+        [DataRow(TABI + FSI + "123456789", 5, 1)]
+        [DataRow(TABI + "123456789", 1, 0)]
+        [DataRow(TABI + TABI + TABI + "123456789", 3, 2)]
+        [DataRow(TABI + "1", 1, 0)]
+        [DataRow(TABI + TABI + TABI + "1", 3, 2)]
+        [DataRow(TABI + TABI + TABI + TABI + TABI + TABI + TABI + "1", 7, 6)]
+        [DataRow(FSI + "text" + FSI, 4, 0)]
+        [DataRow("", null, -1)]
+        [DataRow(FSI + FSI + "12345", 8, 1)]
+        public void DecorateLineTests_IndexTesting_ExpectedBehavior(string text, int? possibleLength, int colorIndex)
+        {
+            var randomDrawIndexCutoff = new Random().Next(2 << 16);
+            decorator.DecorateLine(text, randomDrawIndexCutoff, 0);
 
-			if (possibleLength != null)
-			{
-				backgroundTextIndexDrawerMock.Verify(
-					p => p.DrawBackground(
-						randomDrawIndexCutoff,
-						It.IsIn(possibleLength.Value),
-						rainbowgetter.GetColorByIndex(colorIndex, 0)),
-					Times.Once()
-				);
-			}
-			else
-			{
-				backgroundTextIndexDrawerMock.Verify(
-					p => p.DrawBackground(
-							It.IsAny<int>(),
-							It.IsAny<int>(),
-							It.IsAny<Brush>()
-						),
-					Times.Never()
-				);
-			}
-		}
+            if (possibleLength != null)
+            {
+                backgroundTextIndexDrawerMock.Verify(
+                    p => p.DrawBackground(
+                        randomDrawIndexCutoff,
+                        It.IsIn(possibleLength.Value),
+                        rainbowgetter.GetColorByIndex(colorIndex, 0),
+                        It.IsAny<int>()),
+                    Times.Once()
+                );
+            }
+            else
+            {
+                backgroundTextIndexDrawerMock.Verify(
+                    p => p.DrawBackground(
+                            It.IsAny<int>(),
+                            It.IsAny<int>(),
+                            It.IsAny<Brush>(),
+                        It.IsAny<int>()
+                        ),
+                    Times.Never()
+                );
+            }
+        }
 
-		[DataTestMethod]
-		[DataRow(FSI + FSI + FSI + FSI + FSI + FSI + FSI)]
-		[DataRow(FSI + "dsadsadsa")]
-		[DataRow(FSI + "  dsadsa")]
-		[DataRow(TABI + FSI + "  dsadsa")]
-		[DataRow(FSI + TABI + "  dsadsa")]
-		public void DecorateLineTests_ColorTesting_ExpectedBehavior(string text)
-		{
-			var itCount = text.Length / FSI.Length;
-			var sequence = new MockSequence();
-			for (var i = 0; i < itCount; i++)
-			{
-				backgroundTextIndexDrawerMock.InSequence(sequence).Setup(
-					p => p.DrawBackground(
-						It.IsAny<int>(),
-						It.IsAny<int>(),
-						It.IsAny<Brush>()
-					)
-				);
-			}
+        [DataTestMethod]
+        [DataRow(FSI + FSI + FSI + FSI + FSI + FSI + FSI)]
+        [DataRow(FSI + "dsadsadsa")]
+        [DataRow(FSI + "  dsadsa")]
+        [DataRow(TABI + FSI + "  dsadsa")]
+        [DataRow(FSI + TABI + "  dsadsa")]
+        public void DecorateLineTests_ColorTesting_ExpectedBehavior(string text)
+        {
+            var itCount = text.Length / FSI.Length;
+            var sequence = new MockSequence();
+            for (var i = 0; i < itCount; i++)
+            {
+                backgroundTextIndexDrawerMock.InSequence(sequence).Setup(
+                    p => p.DrawBackground(
+                        It.IsAny<int>(),
+                        It.IsAny<int>(),
+                        It.IsAny<Brush>(),
+                        It.IsAny<int>()
+                    )
+                );
+            }
 
-			decorator.DecorateLine(text, 0);
+            decorator.DecorateLine(text, 0, 0);
 
-			for (var i = 0; i < itCount; i++)
-			{
-				backgroundTextIndexDrawerMock.InSequence(sequence).Setup(
-					p => p.DrawBackground(
-						It.IsAny<int>(),
-						It.IsAny<int>(),
-						rainbowgetter.GetColorByIndex(i, 0)
-					)
-				);
-			}
-		}
+            for (var i = 0; i < itCount; i++)
+            {
+                backgroundTextIndexDrawerMock.InSequence(sequence).Setup(
+                    p => p.DrawBackground(
+                        It.IsAny<int>(),
+                        It.IsAny<int>(),
+                        rainbowgetter.GetColorByIndex(i, 0),
+                        It.IsAny<int>()
+                    )
+                );
+            }
+        }
 
 
-		[DataTestMethod]
-		[DataRow(FSI + FSI + TABI + FSI + " t", 14)]
-		[DataRow(TABI + FSI + " 123456789", 6)]
-		[DataRow(FSI + " text" + FSI, 5)]
-		[DataRow(FSI + FSI + " 12345", 9)]
-		[DataRow("test", null)]
-		public void DecorateLineTests_IndexTesting_ErrorBehaviors(string text, int? endPosition)
-		{
-			var randomDrawIndexCutoff = new Random().Next(2 << 16);
-			decorator.DecorateLine(text, randomDrawIndexCutoff);
+        [DataTestMethod]
+        [DataRow(FSI + FSI + TABI + FSI + " t", 14)]
+        [DataRow(TABI + FSI + " 123456789", 6)]
+        [DataRow(FSI + " text" + FSI, 5)]
+        [DataRow(FSI + FSI + " 12345", 9)]
+        [DataRow("test", null)]
+        public void DecorateLineTests_IndexTesting_ErrorBehaviors(string text, int? endPosition)
+        {
+            var randomDrawIndexCutoff = new Random().Next(2 << 16);
+            decorator.DecorateLine(text, randomDrawIndexCutoff, 0);
 
-			if (endPosition != null)
-			{
-				backgroundTextIndexDrawerMock.Verify(
-					p => p.DrawBackground(
-						randomDrawIndexCutoff,
-						endPosition.Value,
-						It.IsAny<Brush>()),
-					Times.Once()
-				);
-				backgroundTextIndexDrawerMock.Verify(
-					p => p.DrawBackground(
-							It.IsNotIn(new int[] { randomDrawIndexCutoff, endPosition.Value }),
-							It.IsNotIn(4),
-							It.IsAny<Brush>()
-						),
-					Times.Never()
-				);
-			}
-		}
+            if (endPosition != null)
+            {
+                backgroundTextIndexDrawerMock.Verify(
+                    p => p.DrawBackground(
+                        randomDrawIndexCutoff,
+                        endPosition.Value,
+                        It.IsAny<Brush>(),
+                        It.IsAny<int>()),
+                    Times.Once()
+                );
+                backgroundTextIndexDrawerMock.Verify(
+                    p => p.DrawBackground(
+                            It.IsNotIn(new int[] { randomDrawIndexCutoff, endPosition.Value }),
+                            It.IsNotIn(4),
+                            It.IsAny<Brush>(),
+                        It.IsAny<int>()
+                        ),
+                    Times.Never()
+                );
+            }
+        }
 
-		[DataTestMethod]
-		[DataRow(FSI + FSI + TABI + FSI + " t", 14)]
-		[DataRow(TABI + FSI + " 123456789", 6)]
-		[DataRow(FSI + " text" + FSI, 5)]
-		[DataRow(FSI + FSI + " 12345", 9)]
-		public void DecorateLineTests_NoErrorDetection_ErrorBehaviors(string text, int endOffset)
-		{
-			decorator.detectErrors = false;
-			var randomDrawIndexCutoff = new Random().Next(2 << 16);
-			decorator.DecorateLine(text, randomDrawIndexCutoff);
+        [DataTestMethod]
+        [DataRow(FSI + FSI + TABI + FSI + " t", 14)]
+        [DataRow(TABI + FSI + " 123456789", 6)]
+        [DataRow(FSI + " text" + FSI, 5)]
+        [DataRow(FSI + FSI + " 12345", 9)]
+        public void DecorateLineTests_NoErrorDetection_ErrorBehaviors(string text, int endOffset)
+        {
+            decorator.detectErrors = false;
+            var randomDrawIndexCutoff = new Random().Next(2 << 16);
+            decorator.DecorateLine(text, randomDrawIndexCutoff, 0);
 
-			backgroundTextIndexDrawerMock.Verify(
-				p => p.DrawBackground(
-					randomDrawIndexCutoff,
-					endOffset,
-					It.IsAny<Brush>()),
-				Times.Once()
-			);
-		}
-	}
+            backgroundTextIndexDrawerMock.Verify(
+                p => p.DrawBackground(
+                    randomDrawIndexCutoff,
+                    endOffset,
+                    It.IsAny<Brush>(),
+                        It.IsAny<int>()),
+                Times.Once()
+            );
+        }
+    }
 }
